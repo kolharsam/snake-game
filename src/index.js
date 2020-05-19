fba38e5335d823f9444747f2a7e960dab0eb031c
Sameer Kolhar <kolhar730@gmail.com>
2020-05-19 17:07:33 +0530

    Update index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React, {StrictMode} from 'react';
+import React, { StrictMode } from "react";
 // TODO: Remove StrictMode upon completing project
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom";
+import SnakeGame from "./App";
+
+import "./index.css";
 
 ReactDOM.render(
-  <StrictMode> . 
-    <App />
+  <StrictMode>
+    <SnakeGame size={550} />
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

ca2b01303cdfbeb0c469d5ca09083d4e79d57cd4
Sameer Kolhar <kolhar730@gmail.com>
2020-05-20 00:58:37 +0530

    Add utils tests

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,4 +1,4 @@
-import { isEqual, diff } from "./";
+import { isEqual, diff, isValidCell, bitMe } from "./";
 
 test("[1,2,3,4,5,6] and [1,2,3,4,5,6] are equal", () => {
   expect(isEqual([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])).toBe(true);
@@ -6,4 +6,18 @@ test("[1,2,3,4,5,6] and [1,2,3,4,5,6] are equal", () => {
 
 test("diff between [2,3,4,5,6] and [1,1,1,1,1] is [1,2,3,4,5]", () => {
   expect(diff([2, 3, 4, 5, 6], [1, 1, 1, 1, 1])).toStrictEqual([1, 2, 3, 4, 5]);
+});
+
+test("[4,5] is a valid cell in a grid with 30 cells in a row", () => {
+  expect(isValidCell([4, 5], 30)).toBe(true);
+});
+
+test("[4,5] is present within [[4,5], [4,5], [3,5]], hence the snake bit itself", () => {
+  expect(
+    bitMe([
+      [4, 5],
+      [4, 5],
+      [3, 5],
+    ])
+  ).toBe(true);
 });

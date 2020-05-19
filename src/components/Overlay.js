581721ac58748caa4effeac66cb9be7ff7d31032
Sameer Kolhar <kolhar730@gmail.com>
2020-05-20 00:59:32 +0530

    Update Overlay component

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,8 +1,9 @@
+// Overlay component for the game board
 import React from "react";
 
 function Overlay({ heading, children }) {
   return (
-    <div className="snake-app__overlay">
+    <div className="game-board__overlay">
       {heading ? <div className="mb-1 heading">{heading}</div> : null}
       {children}
     </div>
--- src/components/Overlay.test.js
+++ src/components/Overlay.test.js
@@ -1,9 +1,25 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Overlay from "./Overlay";
 import { render } from "@testing-library/react";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Overlay />, div);
+});
 
 it('renders an overlay with heading "Snake Game"', () => {
   const { container } = render(<Overlay heading="Snake Game" />);
 
   expect(container.firstChild.firstChild.textContent).toBe("Snake Game");
+});
+
+it("renders all children in overlay", () => {
+  const { container } = render(
+    <Overlay heading="Snake Game">
+      <p>Hello, world!</p>
+    </Overlay>
+  );
+
+  expect(container.firstChild.hasChildNodes()).toBe(true);
 });

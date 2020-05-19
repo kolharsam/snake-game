4d02d2ae25c74b13e0b4ea80a189d59026406a42
Sameer Kolhar <kolhar730@gmail.com>
2020-05-19 17:07:03 +0530

    Add app constants

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+const DIRECTION = {
+  LEFT: 37,
+  RIGHT: 39,
+  UP: 38,
+  DOWN: 40,
+};
+
+const STATUS = {
+  NO_PLAY: 0,
+  PLAYING: 1,
+  OVER: 2,
+};
+
+const CELL_SIZE = 15;
+
+export { DIRECTION, STATUS, CELL_SIZE };
